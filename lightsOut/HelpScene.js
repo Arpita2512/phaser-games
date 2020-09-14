20854131863f1699003dfc1672c2f6e9f4d87e40
Arpita Saggar <59466546+Arpita2512@users.noreply.github.com>
2020-09-14 14:08:24 +0530

    fix centering

--- lightsOut/HelpScene.js
+++ lightsOut/HelpScene.js
@@ -3,27 +3,31 @@ class HelpScene extends Phaser.Scene{
         super({key: 'HelpScene'});
     }
     create(){
-        this.add.text(170, 120, "Jojo is a very naughty boy!", {
+        const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
+        
+        this.add.text(screenCenterX, 120, "Jojo is a very naughty boy!", {
             fontFamily: 'Lucida Console',
             fontStyle: 'bold',
             fontSize: '28px'
-        });
-        this.add.text(130, 160, " He is playing in all the rooms, \nbut isn't switching off the lights \n         when he leaves.", {
+        }).setOrigin(0.5);
+        
+        this.add.text(screenCenterX, 160, " He is playing in all the rooms, \nbut isn't switching off the lights \n         when he leaves.", {
             fontFamily: 'Lucida Console',
             fontStyle: 'bold',
             fontSize: '28px'
-        });
-        this.add.text(130, 320, " Switch off all the extra lights,\nbut don't turn off the light in\n          Jojo's room!", {
+        }).setOrigin(0.5);
+        
+        this.add.text(screenCenterX, 320, " Switch off all the extra lights,\nbut don't turn off the light in\n          Jojo's room!", {
             fontFamily: 'Lucida Console',
             fontStyle: 'bold',
             fontSize: '28px'
-        });
+        }).setOrigin(0.5);
 
-        this.add.text(300, 500, "Click to begin!", {
+        this.add.text(screenCenterX, 500, "Click to begin!", {
             fontFamily: 'Lucida Console',
             fontStyle: 'bold',
             fontSize: '20px'
-        });
+        }).setOrigin(0.5);
 
         this.input.on('pointerup', () => {
             this.scene.stop('HelpScene');

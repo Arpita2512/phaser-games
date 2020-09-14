1c57163c576aab9472c471b7697617c14e51646e
Arpita Saggar <59466546+Arpita2512@users.noreply.github.com>
2020-09-14 13:09:51 +0530

    fix centering

--- fluffyBird/EndScene.js
+++ fluffyBird/EndScene.js
@@ -13,26 +13,29 @@ class EndScene extends Phaser.Scene{
     }
     create(){
         this.bg = this.add.tileSprite(400, 240, 800, 480, 'bg_img').setScale(1.2, 1.5);
-        this.add.image(410, 200, 'gameover');
-
+        
         if(score >= 2)
             score -= 2;
-
-        this.add.text(330, 300, `Score: ${score}`, {
+        
+        const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
+        this.add.image(screenCenterX, 200, 'gameover').setOrigin(0.5);
+        
+        this.add.text(screenCenterX, 300, `Score: ${score}`, {
             fontFamily: 'Comic Sans MS',
             fontStyle: 'bold',
             fontSize: '32px',
             stroke: '#000000',
             strokeThickness: 10
-        });
+        }).setOrigin(0.5);
 
-        let again = this.add.text(200, 400, 'Click Here to Try Again!', {
+        let again = this.add.text(screenCenterX, 400, 'Click Here to Try Again!', {
             fontFamily: 'Comic Sans MS',
             fontStyle: 'bold',
             fontSize: '32px',
             stroke: '#000000',
             strokeThickness: 10
-        });
+        }).setOrigin(0.5);
+        
         again.setInteractive();
         again.on('pointerup', () => {
             this.scene.stop('EndScene');

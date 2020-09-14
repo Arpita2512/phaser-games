a0161bf57afde3c16b2135e87d88b94d3ccccedd
Arpita Saggar <59466546+Arpita2512@users.noreply.github.com>
2020-09-14 14:29:24 +0530

    fix centering

--- virusDodge/EndScene.js
+++ virusDodge/EndScene.js
@@ -11,23 +11,27 @@ class EndScene extends Phaser.Scene{
         this.load.image('gameOver', 'assets/gameOver.png');
     }
     create(){
-        this.add.image(280, 180, 'gameOver').setScale(.8);
-        this.add.text(200, 320, `Game Over!\n\nScore: ${score}`, {
+        const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
+        this.add.image(screenCenterX, 180, 'gameOver').setScale(.8).setOrigin(0.5);
+        
+        this.add.text(screenCenterX, 320, `Game Over!\n\nScore: ${score}`, {
             fontFamily: 'Comic Sans MS',
             fontStyle: 'bold',
             fontSize: '24px',
             color: '#DBFEE7',
             stroke: '#000000',
             strokeThickness: 7
-        });
-        this.add.text(180, 480, 'Click to restart!', {
+        }).setOrigin(0.5);
+        
+        this.add.text(screenCenterX, 480, 'Click to restart!', {
             fontFamily: 'Comic Sans MS',
             fontStyle: 'bold',
             fontSize: '24px',
             color: '#DBFEE7',
             stroke: '#000000',
             strokeThickness: 7
-        });
+        }).setOrigin(0.5);
+        
         this.input.on('pointerup', () => {
             this.scene.stop('EndScene');
             this.scene.start('GameScene');

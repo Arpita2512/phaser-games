f3c255a62a7d414b6fd9f3fba71039d7705f83b7
Arpita Saggar <59466546+Arpita2512@users.noreply.github.com>
2020-09-14 12:25:45 +0530

    Update StartScene.js

--- fluffyBird/StartScene.js
+++ fluffyBird/StartScene.js
@@ -9,25 +9,24 @@ class StartScene extends Phaser.Scene{
         this.load.audio('bgMusic', 'assets/golucky.mp3');
     }
     create(){
-        //this.bg = this.add.image(0, 0, 'bg_img').setScale(1.2, 1.5).setOrigin(0, 0);
         this.bg = this.add.tileSprite(400, 240, 800, 480, 'bg_img').setScale(1.2, 1.5);
-        //this.grass = this.add.tileSprite
         
         var music = this.sound.add('bgMusic');
-        
         music.play({
             loop: true
         });
-               
-        this.add.text(290, 400, 'Click to play!', {
+        
+        const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;               
+        this.add.text(screenCenterX, 400, 'Click to play!', {
             fontFamily: 'Comic Sans MS',
             fontStyle: 'bold',
             fontSize: '32px',
-            //color: '#000000',
             stroke: '#000000',
             strokeThickness: 10
-        });
-        this.add.image(420, 200, 'logo');
+        }).setOrigin(0.5);
+        
+        this.add.image(screenCenterX, 200, 'logo').setOrigin(0.5);
+        
         this.input.on('pointerup', () => {
             this.scene.stop('StartScene');
             this.scene.start('GameScene');
